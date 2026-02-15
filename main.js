37aa3b2c7e219233bf2abc3c0ad1b6c6c8a1d1ac
Amirhossein8118 <amir15sadeghi1384@gmail.com>
2026-02-15 16:28:19 +0330

    feat: implement dark mode toggle

---
+++ main.js
@@ -0,0 +1,28 @@
+let themeController = document.querySelector("#theme-controller");
+
+window.addEventListener("load", () => {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    document.documentElement.classList.toggle(
+        "dark",
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    );
+
+    // set the initial state of the toggle based on the current theme
+    if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+        themeController.checked = true;
+    }
+})
+
+themeController.addEventListener("change", (e) => {
+    if (e.target.checked) {
+        // Whenever the user explicitly chooses dark mode
+        localStorage.theme = "dark";
+        document.documentElement.classList.toggle("dark", true);
+    } else {
+        // Whenever the user explicitly chooses light mode
+        localStorage.theme = "light";
+        document.documentElement.classList.toggle("dark", false);
+    }
+});
+
